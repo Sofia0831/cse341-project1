ab795f1e75e708c0bdae0363764778ad944df715
sofiapantas <sofiasarabia0831@gmail.com>
2025-07-11 17:07:13 +0800

    week 2 contacts project part 2

--- routes/contacts.js
+++ routes/contacts.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const contactController = require('../controllers/contactsController');
 
 router.get('/', contactController.getAll);
-router.get('/:id', contactController.getSingle)
+router.get('/:id', contactController.getSingle);
+
+// Week 2 routes
+router.post('/', contactController.createContact);
+router.put('/:id', contactController.updateContact);
+router.delete('/:id', contactController.deleteContact);
 
 module.exports = router;
